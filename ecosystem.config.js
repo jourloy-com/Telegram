429d4de9636cdaa0aef4d3feed170fa0af89ce58
Jourloy <jourloy@yandex.ru>
2022-08-27 00:40:12 +0300

    🔨 Fix Ci/CD

--- ecosystem.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	apps: {
-		script: `dist/main.js`,
-		// Specify which folder to watch
-		watch: [`src`],
-		// Specify delay between watch interval
-		watch_delay: 1000,
-	},
-};
